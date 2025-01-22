c827ffe86e12d3035046f2917658f5ff98da5133
Nglicloud <81097125+Nglicloud@users.noreply.github.com>
2025-01-22 16:04:45 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,10 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-import os
+from flask import Flask
 
-def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "Arrc"
-    message = "Hello, " + name + "!\n"
-    return Response(message)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to AWS App Runner! Your Python application is running successfully."
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+    app.run(host='0.0.0.0', port=8080)
